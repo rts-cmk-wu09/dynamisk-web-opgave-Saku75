1d8b071eaa73ef232e2ac940678462f2895ea7ac
Lukas Mann <lukas@lvmann.dk>
2023-02-14 11:03:57 +0100

    Created services section

--- js/modules/services.js
+++ js/modules/services.js
@@ -1,1 +1,22 @@
-export default function main() {}
+import createElement from "../components/createElement.js";
+import createCards from "../components/createCards.js";
+
+export default function main(data) {
+	const services = document.querySelector(".services");
+
+	createElement("link", document.head, (stylesheet) => {
+		stylesheet.rel = "stylesheet";
+		stylesheet.href = "/css/modules/services.css";
+	});
+
+	const mappedData = data.map((item) => {
+		return {
+			image: item.illustration,
+			headline: item.headline,
+			text: item.text,
+			linkText: item.linktext,
+		};
+	});
+
+	createCards(mappedData, services);
+}
