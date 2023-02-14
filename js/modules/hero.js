75cd735b9c1b49ef37b83a0413bc467829d0299d
Lukas Mann <lukas@lvmann.dk>
2023-02-14 10:36:10 +0100

    Created hero section

--- js/modules/hero.js
+++ js/modules/hero.js
@@ -1,2 +1,36 @@
-export default function main() {
+import createElement from "../components/createElement.js";
+
+export default function main(data) {
+	const hero = document.querySelector(".hero");
+
+	createElement("link", document.head, (stylesheet) => {
+		stylesheet.rel = "stylesheet";
+		stylesheet.href = "/css/modules/hero.css";
+	});
+
+	createElement("nav", hero, (navbar) => {
+		createElement("a", navbar, (logoLink) => {
+			createElement("img", logoLink).src = "/img/logo.svg";
+		}).href = "/";
+		createElement("button", navbar, (button) => {
+			createElement("img", button).src = "/img/arrowDown.svg";
+			createElement("img", button).src = "/img/profile.svg";
+		});
+	});
+
+	createElement("img", hero, (image) => {
+		image.src = data.image;
+	});
+
+	createElement("div", hero, (box) => {
+		createElement("h1", box).innerHTML = data.headline.replace(
+			"save your time",
+			"<span>save your time</span>"
+		);
+		createElement("p", box).innerText = data.copy;
+		createElement("button", box, (button) => {
+			createElement("img", button).src = data.icon;
+			button.innerHTML += "Explore";
+		});
+	});
 }
