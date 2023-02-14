3d95a1cee8ac1f1f3128220f3c897882abcb7d91
Lukas Mann <lukas@lvmann.dk>
2023-02-14 10:37:23 +0100

    Created createCards module

---
+++ js/components/createCards.js
@@ -0,0 +1,16 @@
+import createElement from "./createElement.js";
+
+export default function createCards(data, parent, callback) {
+	data.forEach((element) => {
+		createElement("section", parent, (card) => {
+			if (element.image) createElement("img", card).src = element.image;
+			if (element.headline)
+				createElement("h3", card).innerText = element.headline;
+			if (element.text) createElement("p", card).innerText = element.text;
+			if (element.linkText)
+				createElement("a", card).innerText = element.linkText;
+			if (callback) callback(card);
+			console.log(card);
+		});
+	});
+}
