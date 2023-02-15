c16abecd400002842a0d2bc7d8195d79ae373568
Lukas Mann <lukas@lvmann.dk>
2023-02-15 08:19:47 +0100

    Created advantages section

--- js/modules/advantages.js
+++ js/modules/advantages.js
@@ -1,1 +1,25 @@
-export default function main() {}
+import createElement from "../components/createElement.js";
+import createCards from "../components/createCards.js";
+
+export default function main(data) {
+	const advantages = document.querySelector(".advantages");
+
+	createElement("link", document.head, (stylesheet) => {
+		stylesheet.rel = "stylesheet";
+		stylesheet.href = "/css/modules/advantages.css";
+	});
+
+	createElement("h2", advantages).innerText = "Our Advantages";
+
+	const mappedData = data.map((item) => {
+		return {
+			image: item.icon,
+			headline: item.headline,
+			text: item.text,
+		};
+	});
+
+	createElement("div", advantages, (cards) => {
+		createCards(mappedData, cards);
+	});
+}
