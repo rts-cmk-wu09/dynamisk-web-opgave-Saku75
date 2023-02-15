26312897ca404979ba3fa6566a7421b766fe6a2b
Lukas Mann <lukas@lvmann.dk>
2023-02-15 08:20:06 +0100

    Created sites section

--- js/modules/sites.js
+++ js/modules/sites.js
@@ -1,1 +1,33 @@
-export default function main() {}
+import createElement from "../components/createElement.js";
+import createCards from "../components/createCards.js";
+
+export default function main(data) {
+	const sites = document.querySelector(".sites");
+
+	createElement("link", document.head, (stylesheet) => {
+		stylesheet.rel = "stylesheet";
+		stylesheet.href = "/css/modules/sites.css";
+	});
+
+	createElement("div", sites, (headline) => {
+		createElement("h2", headline).innerText = data.headline;
+		createElement("p", headline).innerText = data.text;
+		createElement("button", headline, (btn) => {
+			createElement("img", btn).src = data.btnicon;
+			btn.innerHTML += "Start";
+		});
+	});
+
+	const mappedData = data.places.map((item) => {
+		return {
+			image: item.img,
+			headline: item.name,
+			text: item.city,
+			linkText: "View the Site",
+		};
+	});
+
+	createElement("div", sites, (cards) => {
+		createCards(mappedData, cards);
+	});
+}
